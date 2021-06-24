421571102df3efccca5af8ce4a1d89726b1aa86e
Juan Antonio Ceballos <jacu29@gmail.com>
2021-06-24 15:28:43 -0500

    Cambia de color por categoría

--- main.js
+++ main.js
@@ -1,140 +1,244 @@
-let user = document.getElementById("user");
-let coverPage = document.getElementById("cover-page");
-let triviaForm = document.getElementById("trivia");
-let questionsPage = document.getElementById("containerQuesiton");
-let questionsContainer = document.getElementById("questionsContent");
-let amount = document.getElementById("amount");
-let category = document.getElementById("category");
-let difficulty = document.getElementById("difficulty");
-let type = document.getElementById("type");
-let answers = document.getElementsByClassName("answer");
-
 let userName;
+const form = document.getElementById("trivia");
+const amount = document.getElementById("amount");
+const category = document.getElementById("category");
+const difficulty = document.getElementById("difficulty");
+const type = document.getElementById("type");
+const board = document.getElementById("table-scores");
+
+const blue = "#0984e3";
+const violet = "#6c5ce7";
+const yellow = "#fdcb6e";
+const pink = "#fd79a8";
+const green = "#00b894";
+const orange = "#e17055";
+
 let questions;
-let questionIndex = 0;
 let correct_index_answer;
-let score = 0;
+let indexQuestion;
+let score;
 
-const renderScores = () => {
-  let scores = JSON.parse(localStorage.getItem("scores"));
-  let score = document.getElementById("scores");
+async function getApiData(e) {
+  e.preventDefault();
+  let url = `https://opentdb.com/api.php?amount=${amount.value}&category=${category.value}&difficulty=${difficulty.value}&type=${type.value}`;
 
-  score.innerHTML = "";
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    questions = await data.results;
+  } catch (error) {
+    console.log("Error", error);
+  }
 
-  if (scores !== null) {
-    for (let i = 0; i < 10; i++) {
-      let { name, points } = scores[i];
-      score.innerHTML = `
-            <div class="player">
-                <i class="fas fa-caret-right"></i>
-                <h3>${name}</h3>
-                <h3>${points}</h3>
-            </div>
-            `;
+  startGame();
+}
+
+function renderScores() {
+  let players = JSON.parse(localStorage.getItem("players"));
+
+
+  board.innerHTML = "";
+
+  if (players !== null) {
+    if (players.length > 10) {
+      for (let i = 0; i < 10; i++) {
+        let {userName,score} = players[i];
+        board.innerHTML += `
+        <div class="player">
+          <i class="fas fa-caret-right"></i>
+          <h3>${userName}</h3>
+          <p>${score}</p>
+        </div>
+        `     
+      }
+    } else {
+      for (let i = 0; i < players.length; i++) {
+        let {userName,score} = players[i];
+        board.innerHTML += `
+        <div class="player">
+          <i class="fas fa-caret-right"></i>
+          <h3>${userName}</h3>
+          <p>${score}</p>
+        </div>
+        `     
+      }
     }
   }
 };
 
-const getApiData = async (e) => {
-  e.preventDefault();
+const selectColor = (value) => {
+  let color;
+  if (value == 22) {
+    color = blue;
+  } else if (
+    value == 10 ||
+    value == 12 ||
+    value == 13 ||
+    value == 25 ||
+    value == 9
+  ) {
+    color = violet;
+  } else if (value == 20 || value == 23 || value == 24) {
+    color = yellow;
+  } else if (
+    value == 11 ||
+    value == 14 ||
+    value == 26 ||
+    value == 28 ||
+    value == 29 ||
+    value == 31 ||
+    value == 32
+  ) {
+    color = pink;
+  } else if (value == 17 || value == 18 || value == 19 || value == 27) {
+    color = green;
+  } else if (value == 15 || value == 16 || value == 21 || value == 30) {
+    color = green;
+  } else {
+    color = "#8395a7";
+  }
 
-  let url = `https://opentdb.com/api.php?amount=${amount.value}&category=${category.value}&difficulty=${difficulty.value}&type=${type.value}`;
+  document.body.style.setProperty("--backgroundColor", color);
+  document.body.style.setProperty("--option", color);
+};
 
-  response = await fetch(url);
-  data = await response.json();
-  questions = data.results;
-  startGame();
+const setScore = () => {
+  document.getElementById("score").innerText = score;
+};
+
+const setBoard = () => {
+  document.getElementById("categoryName").innerHTML =
+    category.options[document.getElementById("category").selectedIndex].text;
+  document.getElementById("name").innerText = userName;
+  setScore();
+  document.getElementById("totalQuestions").innerText = amount.value;
+  document.getElementById("currentQuestion").innerHTML = indexQuestion + 1;
+};
+
+const setQuestionField = () => {
+  let currentQuestion = questions[indexQuestion];
+
+  document.getElementById("question").innerHTML = currentQuestion.question;
+
+  if (currentQuestion.incorrect_answers.length == 1) {
+    document.getElementById("1").innerText = "True";
+    document.getElementById("2").innerText = "False";
+    document.getElementById("3").style.display = "none";
+    document.getElementById("4").style.display = "none";
+
+    currentQuestion.correct_answer === "True"
+      ? (correct_index_answer = 1)
+      : (correct_index_answer = 2);
+  } else {
+    document.getElementById("1").style.display = "Block";
+    document.getElementById("2").style.display = "Block";
+    document.getElementById("3").style.display = "Block";
+    document.getElementById("4").style.display = "Block";
+
+    correct_index_answer = Math.floor(Math.random() * 4) + 1;
+    document.getElementById(correct_index_answer).innerHTML =
+      currentQuestion.correct_answer;
+    let j = 0;
+    for (let i = 1; i <= 4; i++) {
+      if (i === correct_index_answer) continue;
+      document.getElementById(i).innerHTML =
+        currentQuestion.incorrect_answers[j];
+      j++;
+    }
+  }
+};
+
+const endGame = () => {
+  document.getElementById("game").style.display = "none";
+  document.getElementById("endGame").style.display = "flex";
+
+  document.getElementById("results").innerHTML = `
+  <h3>${userName}</h3>
+  <h4><em>Score:</em> ${score}</h4>
+  `;
+
+  
+  const player = {
+    userName,
+    score 
+  }
+
+  if (localStorage.getItem('players')===null){
+    let players = [];
+    players.push(player);
+    localStorage.setItem('players',JSON.stringify(players));
+} else {
+    let players = JSON.parse(localStorage.getItem('players'));
+    players.push(player)
+    localStorage.setItem('players',JSON.stringify(players))
+}
+
+  document.getElementById("begin").addEventListener("click", () => {
+    trivia();
+    document.getElementById("endGame").style.display = "none";
+  });
 };
 
 const startGame = () => {
-    triviaForm.style.display = 'none';
-    questionsPage.style.display = 'flex';
-    // console.log(category.value);
-    // console.log(questions);
-    document.getElementById('categoryName').innerText = category.options[document.getElementById('category').selectedIndex].text;
-    document.getElementById('userName').innerText = userName;
-    document.getElementById('userPoints').innerText = score;
-    document.getElementById('num_questions').innerText = amount.value;
-    document.getElementById('question_index').innerText = questionIndex + 1;
+  document.getElementById("coverPage").style.display = "none";
+  document.getElementById("game").style.display = "grid";
 
-    
-    let currentQuestion = questions[questionIndex];
-    document.getElementById("questionName").innerHTML = `<p>${currentQuestion.question}</p>`;
+  selectColor(category.value);
+  setBoard();
+  setQuestionField();
 
-    if (currentQuestion.incorrect_answers.length == 1) {
-        document.getElementById("1").innerText = "True";
-        document.getElementById("2").innerText = "False";
-        document.getElementById("3").style.display = "none";
-        document.getElementById("4").style.display = "none";
-        if (currentQuestion.correct_answer === "True") correct_index_answer = 1;
-        else correct_index_answer = 2;
-      } else {
-        document.getElementById("1").style.display = "Block";
-        document.getElementById("2").style.display = "Block";
-        document.getElementById("3").style.display = "Block";
-        document.getElementById("4").style.display = "Block";
-    
-        correct_index_answer = Math.floor(Math.random() * 4) + 1;
-        document.getElementById(correct_index_answer).innerText = currentQuestion.correct_answer;
-        console.log(correct_index_answer);
-        let j = 0;
-        for (let i = 1; i <= 4; i++) {
-          if (i === correct_index_answer) continue;
-          document.getElementById(i).innerText = currentQuestion.incorrect_answers[j];
-          j++;
-        }
+  let buttons = document.getElementById("buttons");
+  // let questionNum = questions.length
+
+  buttons.querySelectorAll("button").forEach((button) => {
+    button.addEventListener("click", () => {
+      if (button.id == correct_index_answer) {
+        score = score + 1;
+        setScore();
       }
+
+      indexQuestion = indexQuestion + 1;
+      if (indexQuestion === questions.length) {
+        endGame();
+      } else {
+        setQuestionField();
+        document.getElementById("currentQuestion").innerHTML =
+          indexQuestion + 1;
+      }
+    });
+  });
+
+  document.getElementById("trivia").reset();
 };
 
+const trivia = () => {
+  indexQuestion = 0;
+  score = 0;
+  document.body.style.setProperty("--backgroundColor", "#2f3542");
+  renderScores();
+  document.getElementById("coverPage").style.display = "grid";
+  document.getElementById("first").style.display = "flex";
 
-const selectAnswer = id => {
-    let answerId = id;
-    console.log(answerId);
-    if (answerId == correct_index_answer) {
-      score = score + 1;
-      console.log("Respuesta correcta");
-    } else {
-      console.log("Respuesta incorrecta");
-    }
-    // console.log(questionIndex == amount.value);
-  
-    if (questionIndex < amount.value - 1) {
-      questionIndex++;
-      startGame();
-    } else if (questionIndex == amount.value - 1) {
-      showResults(score);
-    }
-  };
-  
-  const showResults = () => {
-    console.log(`Juego terminado`);
+  document.getElementById("trivia").style.display = "none";
+  document.getElementById("user").style.display = "none";
 
-  };
-  
+  document.getElementById("ingresar").addEventListener("click", () => {
+    document.getElementById("first").style.display = "none";
+    document.getElementById("user").style.display = "flex";
+  });
 
-document.getElementById("welcome").addEventListener("click", () => {
-  user.style.display = "flex";
-});
+  document.getElementById("cancel").addEventListener("click", () => {
+    document.getElementById("first").style.display = "flex";
+    document.getElementById("user").style.display = "none";
+  });
 
-document.getElementById("cancel").addEventListener("click", () => {
-  user.style.display = "none";
-});
+  document.getElementById("continue").addEventListener("click", () => {
+    userName = document.getElementById("userName").value;
+    document.getElementById("user").style.display = "none";
+    document.getElementById("trivia").style.display = "flex";
+  });
 
-document.getElementById("continue").addEventListener("click", () => {
-  user.style.display = "none";
-  coverPage.style.display = "none";
-  userName = document.getElementById("name").value;
-  document.getElementById("triviaForm").reset();
-  triviaForm.style.display = "flex";
-  // console.log(user);
-});
+  document.getElementById("start").addEventListener("click", getApiData);
+};
 
-  
-for (let i = 0; i < answers.length; i++) {
-    const element = answers[i];
-    element.addEventListener("click", () => selectAnswer(element.id));
-  }
-
-
-document.getElementById("triviaForm").addEventListener("submit", getApiData);
-
+trivia();
